48b42aa1e01fc058de468e6f21c653ed1c149a84
Yun-tellije <dbswjddms517@naver.com>
2024-03-07 23:33:27 +0900

    add: MemberServiceImpl - 생성자로 레포지터리 따로 받아줌

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -2,7 +2,11 @@ package hello.core.member;
 
 public class MemberServiceImpl implements MemberService{ // 구현체가 하나일 때는 관례상 인터페이스 이름 뒤에 임플이라고 씀(Impl)
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
