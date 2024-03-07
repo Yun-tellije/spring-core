47e9135533a61c247d4007c3c15af1c1397b54bd
Yun-tellije <dbswjddms517@naver.com>
2024-03-08 01:03:49 +0900

    comment: 의존 관계 변경에 대한 설명

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -9,6 +9,8 @@ import hello.core.order.OrderServiceImpl;
 
 public class AppConfig {
 
+    // 생성한 객체 인스턴스의 참조(레퍼런스)를 생성자를 통해서 주입해줌
+
     public MemberService memberService() {
         return new MemberServiceImpl(new MemoryMemberRepository());
     }
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,6 +3,8 @@ package hello.core.member;
 public class MemberServiceImpl implements MemberService{ // 구현체가 하나일 때는 관례상 인터페이스 이름 뒤에 임플이라고 씀(Impl)
 
     private final MemberRepository memberRepository;
+    // MemoryMemberRepository 의존 x -> MemberRepository만 의존하도록 변경
+    // 어떤 구현 객체를 주입할지는 오직 AppConfig(외부)에서 결정: DI
 
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
