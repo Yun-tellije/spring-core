25ece13941e430d059a3a54d127d7cbc6d6334d9
Yun-tellije <dbswjddms517@naver.com>
2024-03-10 00:50:09 +0900

    test: ApplicationContextBasicFindTest - 구체 타입으로 조회하는 테스트

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -27,4 +27,11 @@ public class ApplicationContextBasicFindTest {
         MemberService memberService = ac.getBean(MemberService.class); // 같은 타입이 여러 개일 경우는 곤란
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
     }
+
+    @Test
+    @DisplayName("구체 타입으로 조회")
+    void findBeanByName2() {
+        MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class); // 인터페이스가 아닌 구현체로 조회 가능 | 하지만 역할에 의존하는 게 좋다(이거는 구현에 의존)
+        assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
 }
