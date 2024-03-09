6e8bed0c0d727cba6ca81791582bd4c6a32e13d9
Yun-tellije <dbswjddms517@naver.com>
2024-03-10 00:53:48 +0900

    test: ApplicationContextBasicFindTest - 빈 이름으로 조회 실패하는 테스트

--- src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beanfind/ApplicationContextBasicFindTest.java
@@ -6,9 +6,11 @@ import hello.core.member.MemberServiceImpl;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class ApplicationContextBasicFindTest {
 
@@ -33,5 +35,13 @@ public class ApplicationContextBasicFindTest {
     void findBeanByName2() {
         MemberService memberService = ac.getBean("memberService", MemberServiceImpl.class); // 인터페이스가 아닌 구현체로 조회 가능 | 하지만 역할에 의존하는 게 좋다(이거는 구현에 의존)
         assertThat(memberService).isInstanceOf(MemberServiceImpl.class);
+    }
+
+    @Test
+    @DisplayName("빈 이름으로 조회X")
+    void findBeanByNameX() {
+//        MemberService memberService = ac.getBean("xxxx", MemberService.class);
+        assertThrows(NoSuchBeanDefinitionException.class,
+                () -> ac.getBean("xxxxx", MemberService.class)); // 오른쪽 로직을 실행했을 때 예외가 터지면 성공
     }
 }
