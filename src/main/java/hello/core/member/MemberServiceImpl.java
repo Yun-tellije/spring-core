14cbe89e57468e0fcbdfef51436e595970358e60
Yun-tellije <dbswjddms517@naver.com>
2024-03-16 23:13:42 +0900

    @Component, @Autowired 추가

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,11 +1,16 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService{ // 구현체가 하나일 때는 관례상 인터페이스 이름 뒤에 임플이라고 씀(Impl)
 
     private final MemberRepository memberRepository;
     // MemoryMemberRepository 의존 x -> MemberRepository만 의존하도록 변경
     // 어떤 구현 객체를 주입할지는 오직 AppConfig(외부)에서 결정: DI
 
+    @Autowired // ac.getBean(MemberRepository.class) | 자동으로 이 코드가 들어감
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -3,7 +3,10 @@ package hello.core.order;
 import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class OrderServiceImpl implements OrderService{
 
 
@@ -12,6 +15,7 @@ public class OrderServiceImpl implements OrderService{
     private final MemberRepository memberRepository;
     private DiscountPolicy discountPolicy; // 구현체에 의존하지 않고 인터페이스에 의존
 
+    @Autowired
     public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
