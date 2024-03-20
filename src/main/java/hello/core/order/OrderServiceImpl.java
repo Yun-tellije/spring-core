b831e6e9b6d71e1dad67cb8c44e0a737149cc158
Yun-tellije <dbswjddms517@naver.com>
2024-03-21 00:07:40 +0900

    조회 빈이 2개일 때 해결 방법 - @Primary 사용

--- src/main/java/hello/core/order/OrderServiceImpl.java
+++ src/main/java/hello/core/order/OrderServiceImpl.java
@@ -5,7 +5,6 @@ import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -30,7 +29,7 @@ public class OrderServiceImpl implements OrderService{
 //    }
 
      @Autowired //생성자 주입
-    public OrderServiceImpl(MemberRepository memberRepository, @Qualifier("mainDiscountPolicy") DiscountPolicy discountPolicy) {
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) {
         this.memberRepository = memberRepository;
         this.discountPolicy = discountPolicy;
     }
